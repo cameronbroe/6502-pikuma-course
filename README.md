726d77b42331d9f3a1ef00e43a8f4287fc72dce2
Cameron Roe <cameron@cameronbroe.com>
2023-07-07 21:00:13 -0500

    Initial cleanmem program from Pikuma course

---
+++ README.md
@@ -0,0 +1,3 @@
+# 6502 Course Stuff
+
+This repo contains my exercises and notes from going through the 6502 Assembler course for the Atari VCS on Pikuma: https://pikuma.com/courses/learn-assembly-language-programming-atari-2600-games
---
+++ exercises/cleanmem/cleanmem.asm
@@ -0,0 +1,30 @@
+    processor 6502
+
+    seg code
+    org $F000               ; Define code origin at $F000
+
+Start:
+    sei                     ; Disable interrupts
+    cld                     ; Disable BCD decimal math mode
+    ldx #$FF                ; Load X register with value #$FF
+    txs                     ; Transfer X register to stack pointer
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Clear the Page Zero region ($00 to $FF)
+; Clear all of RAM and the TIA region
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #0                  ; A = #0
+    ldx #$FF                ; X = #$FF
+
+MemLoop:
+    sta $0,X                ; Store A into mem addr $0 + X
+    dex                     ; X--
+    bne MemLoop             ; while X != 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fill ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFFC
+    .word Start             ; Reset vector at $FFFC to program start
+    .word Start             ; Interrupt vector at $FFFE (unused in VCS)
